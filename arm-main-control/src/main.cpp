1bfcbc0f0c567d4c2a13f054358fe85d6042cb9d
TiltedPhosphor <voss.till@gmail.com>
2024-11-16 20:41:45 +0100

    coded

--- arm-main-control/src/main.cpp
+++ arm-main-control/src/main.cpp
@@ -4,18 +4,41 @@
 u_int8_t servoPins[] = {4,5,6,7};
 
 Servo servos[4];
+u_int16_t analogValues[4];
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  for (int i = 0; i < 4; i++) {
+  Serial.println("Starting...");
+  for(int i = 0; i < 4; i++) {
     servos[i].attach(servoPins[i]);
-    servos[i].write(90);
   }
-  
 
+}
+
+u_int8_t mapValue(u_int16_t value) {
+  return map(value, 0, 4095, 0, 180);
+}
+
+void moveServos() {
+  for(int i = 0; i < 4; i++) {
+    Serial.print(analogValues[i]);
+    Serial.print(" => ");
+    Serial.print(mapValue(analogValues[i]));
+    Serial.print(", ");
+    servos[i].write(mapValue(analogValues[i]));
+  }
+  Serial.println(" moved");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  while(Serial.available() > 0) {
+    for(int i = 0; i < 4; i++) {
+      analogValues[i] = Serial.parseInt();
+      Serial.print("Analog value read ");
+      Serial.println(analogValues[i]);
+    }
+    moveServos();
+  }
 }
