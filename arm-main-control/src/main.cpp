41596e9f82a469c5d14a2074c8bed95340dc2061
TiltedPhosphor <voss.till@gmail.com>
2024-11-16 19:57:25 +0100

    added library, diagram and code

--- arm-main-control/src/main.cpp
+++ arm-main-control/src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
 
-// put function declarations here:
-int myFunction(int, int);
+u_int8_t servoPins[] = {4,5,6,7};
+
+Servo servos[4];
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  for (int i = 0; i < 4; i++) {
+    servos[i].attach(servoPins[i]);
+    servos[i].write(90);
+  }
+  
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
