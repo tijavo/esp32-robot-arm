4c572a489283d38be2b3a59e8acf05f2ea2a1040
TiltedPhosphor <voss.till@gmail.com>
2024-11-16 16:33:24 +0100

    added code

--- remote-potentiometer/src/main.cpp
+++ remote-potentiometer/src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
+/*
+ * This ESP32 code is created by esp32io.com
+ *
+ * This ESP32 code is released in the public domain
+ *
+ * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-potentiometer
+ */
 
-// put function declarations here:
-int myFunction(int, int);
+float floatMap(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
+// the setup routine runs once when you press reset:
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(115200);
+  // set the ADC attenuation to 11 dB (up to ~3.3V input)
+  Serial.println("Setting ADC attenuation to 11 dB");
+  analogSetAttenuation(ADC_11db);
+  pinMode(19, OUTPUT);
 }
 
+// the loop routine runs over and over again forever:
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // read the input on analog pin GPIO36:
+  int analogValue = analogRead();
+  // Rescale to potentiometer's voltage (from 0V to 3.3V):
+  float voltage = floatMap(analogValue, 0, 4095, 0, 3.3);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  //toggle led
+  digitalWrite(19, !digitalRead(19));
+
+  // print out the value you read:
+  Serial.print("Analog: ");
+  Serial.print(analogValue);
+  Serial.print(", Voltage: ");
+  Serial.println(voltage);
+  delay(1000);
 }
