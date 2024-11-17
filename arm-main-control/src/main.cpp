c431264a4071310845bb3796198de9a9e0db9bb7
TiltedPhosphor <voss.till@gmail.com>
2024-11-17 14:09:52 +0100

    added COde

--- arm-main-control/src/main.cpp
+++ arm-main-control/src/main.cpp
@@ -1,10 +1,12 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
+#include <list>
 
 u_int8_t servoPins[] = {4,5,6,7};
 
 Servo servos[4];
-u_int16_t analogValues[4];
+std::array<u_int16_t, 4> analogValues;
+std::list<std::array<u_int16_t, 4>> savedPoints;
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,19 +15,33 @@ void setup() {
   for(int i = 0; i < 4; i++) {
     servos[i].attach(servoPins[i]);
   }
-
 }
 
 u_int8_t mapValue(u_int16_t value) {
   return map(value, 0, 4095, 0, 180);
 }
 
+void savePoint() {
+  savedPoints.push_back(analogValues);
+}
+
+void goThroughPoints() {
+  for(auto point : savedPoints) {
+    analogValues = point;
+    moveServos();
+    delay(1000);
+  }
+}
+
 void moveServos() {
   for(int i = 0; i < 4; i++) {
-    Serial.print(analogValues[i]);
+    Serial.print("Servo ");
+    Serial.print(i);
     Serial.print(" => ");
+    Serial.print(analogValues[i]);
+    Serial.print("(");
     Serial.print(mapValue(analogValues[i]));
-    Serial.print(", ");
+    Serial.print("°), ");
     servos[i].write(mapValue(analogValues[i]));
   }
   Serial.println(" moved");
@@ -35,9 +51,9 @@ void loop() {
   // put your main code here, to run repeatedly:
   while(Serial.available() > 0) {
     for(int i = 0; i < 4; i++) {
+      Serial.print("Enter Number for Servo ");
+      Serial.println(i);
       analogValues[i] = Serial.parseInt();
-      Serial.print("Analog value read ");
-      Serial.println(analogValues[i]);
     }
     moveServos();
   }
